ac9019bae6fa4c0cdb7640bbb6928702ed6e5881
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-04 12:01:28 +1000

    added a small executable that prints out the version number

---
+++ tools/jenic/main.cpp
@@ -0,0 +1,6 @@
+#include "jenic_config.h"
+#include <iostream>
+
+int main (int argc, char* argv []) {
+std::cout << "Jenic Version" << JENIC_MAJOR_VERSION << "." << JENIC_MINOR_VERSION << "." << JENIC_PATCH_VERSION << std::endl;
+}
