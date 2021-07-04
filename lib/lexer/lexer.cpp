326c0c2a38be9c2bd82256334bfc5080aad8d277
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-05 09:51:07 +1000

    made the program read a line from stdin for lexing (lexer not implemented yet)

--- lib/lexer/lexer.cpp
+++ lib/lexer/lexer.cpp
@@ -1,11 +1,12 @@
 #include "jenic/lexer/lexer.h"
 
 namespace jenic {
-    Lexer::Lexer(std::string str) {
+    jenic::Lexer::Lexer(std::string str) {
         input = str;
     }
-    std::vector<jenic::Token> next() {
+    std::vector<jenic::Token> jenic::Lexer::next() {
         std::vector<jenic::Token> result;
         return result;
     }
+
 }
--- tools/jenic/main.cpp
+++ tools/jenic/main.cpp
@@ -1,6 +1,15 @@
 #include "jenic_config.h"
 #include <iostream>
+#include "jenic/lexer/lexer.h"
 
 int main (int argc, char* argv []) {
 std::cout << "Jenic Version" << JENIC_MAJOR_VERSION << "." << JENIC_MINOR_VERSION << "." << JENIC_PATCH_VERSION << std::endl;
+std::string line;
+std::getline(std::cin, line);
+std::cout << "Compiling text from stdin: " << line << std::endl;
+jenic::Lexer lexer(line);
+std::vector<jenic::Token> first = lexer.next();
+for (int i = 0; i < first.size(); i ++) {
+    std::cout << first [i].value << " ";
+}
 }
