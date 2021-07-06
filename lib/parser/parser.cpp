782949ddf1b7c4ceb0a1d0b0d19538b5d44169f4
Jett Thompson <jett.thompson.59@gmail.com>
2021-07-06 14:36:09 +1000

    added implentations of the function supported by parser (but they don't actually do anything yet) and made the parser library a dependency of the jenic executable

--- lib/parser/parser.cpp
+++ lib/parser/parser.cpp
@@ -1,1 +1,9 @@
 #include "jenic/parser/parser.h"
+
+jenic::Parser::Parser(std::vector<jenic::Token> tokens) {
+    this->tokens = tokens;
+}
+jenic::AbstractSyntaxTree jenic::Parser::parse() {
+    jenic::AbstractSyntaxTree tree;
+    return tree;
+}
