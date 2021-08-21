4aed40205cf231312370bccd03a3d113c356cd77
Jett Thompson <jett.thompson.59@gmail.com>
2021-08-21 13:39:17 +1000

    made the main function call the collector

--- tools/jenic/main.cpp
+++ tools/jenic/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "jenic/lexer/lexer.h"
 #include "jenic/parser/parser.h"
+#include "jenic/collector/collector.h"
 
 int main (int argc, char* argv []) {
     std::cout << "Jenic Version" << JENIC_MAJOR_VERSION << "." << JENIC_MINOR_VERSION << "." << JENIC_PATCH_VERSION << std::endl;
@@ -11,9 +12,20 @@ int main (int argc, char* argv []) {
     std::vector<jenic::Token> tokens = lexer.lex();
     jenic::Parser parser(tokens);
     jenic::AbstractSyntaxTree tree = parser.parse();
-    for (int i = 0; i < tree.size(); i ++) {
-        std::cout << tree [i]->toString() << std::endl;
-        delete tree [i];
+    jenic::Collector collector(tree);
+    jenic::Collection* collection = collector.collect();
+    for (int i = 0; i < collection->variables.size(); i ++) {
+        std::cout << collection->variables [i]->toString() << std::endl;
+        delete collection->variables [i];
     }
+    for (int i = 0; i < collection->constants.size(); i ++) {
+        std::cout << collection->constants [i]->toString() << std::endl;
+        delete collection->constants [i];
+    }
+    for (int i = 0; i < collection->functions.size(); i ++) {
+        std::cout << collection->functions[i]->toString() << std::endl;
+        delete collection->functions[i];
+    }
+    delete collection;
 return 0;
 }
